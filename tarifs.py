596a76690d2f1e0e837493b7823c68f5b23afdcf
Guillaume Meister <Wilpapa@users.noreply.github.com>
2020-07-07 15:03:07 +0200

    Update tarifs.py

--- tarifs.py
+++ tarifs.py
@@ -5,27 +5,26 @@
 #        "_id" : ObjectId("5efb1138fdf7f9c40e35388a"),
 #        "start" : ISODate("2020-01-01T00:00:00Z"),
 #        "end" : ISODate("2020-05-20T00:00:00Z"),
-#        "tariff" : 5,
 #        "level" : "A"
 #}
 #
 # output data (intermediate, aggregation pipeline)
-#{ "_id" : ISODate("2020-01-28T00:00:00Z"), "tariffLevel" : "A" }
-#{ "_id" : ISODate("2020-01-29T00:00:00Z"), "tariffLevel" : "A" }
-#{ "_id" : ISODate("2020-01-30T00:00:00Z"), "tariffLevel" : "B" }
-#{ "_id" : ISODate("2020-01-31T00:00:00Z"), "tariffLevel" : "B" }
-#{ "_id" : ISODate("2020-02-01T00:00:00Z"), "tariffLevel" : "C" }
-#{ "_id" : ISODate("2020-02-02T00:00:00Z"), "tariffLevel" : "B" }
-#{ "_id" : ISODate("2020-02-03T00:00:00Z"), "tariffLevel" : "A" }
+#{ "_id" : ISODate("2020-01-28T00:00:00Z"), "policyLevel" : "A" }
+#{ "_id" : ISODate("2020-01-29T00:00:00Z"), "policyLevel" : "A" }
+#{ "_id" : ISODate("2020-01-30T00:00:00Z"), "policyLevel" : "B" }
+#{ "_id" : ISODate("2020-01-31T00:00:00Z"), "policyLevel" : "B" }
+#{ "_id" : ISODate("2020-02-01T00:00:00Z"), "policyLevel" : "C" }
+#{ "_id" : ISODate("2020-02-02T00:00:00Z"), "policyLevel" : "B" }
+#{ "_id" : ISODate("2020-02-03T00:00:00Z"), "policyLevel" : "A" }
 #
-# output data (after final processing of tariff level)
-#{ "_id" : ISODate("2020-01-28T00:00:00Z"), "tariffLevel" : "A" }
-#{ "_id" : ISODate("2020-01-29T00:00:00Z"), "tariffLevel" : "A" }
-#{ "_id" : ISODate("2020-01-30T00:00:00Z"), "tariffLevel" : "B" }
-#{ "_id" : ISODate("2020-01-31T00:00:00Z"), "tariffLevel" : "B" }
-#{ "_id" : ISODate("2020-02-01T00:00:00Z"), "tariffLevel" : "C" }
-#{ "_id" : ISODate("2020-02-02T00:00:00Z"), "tariffLevel" : "B'" }
-#{ "_id" : ISODate("2020-02-03T00:00:00Z"), "tariffLevel" : "A'" }
+# output data (after final processing of policy level)
+#{ "_id" : ISODate("2020-01-28T00:00:00Z"), "policyLevel" : "A" }
+#{ "_id" : ISODate("2020-01-29T00:00:00Z"), "policyLevel" : "A" }
+#{ "_id" : ISODate("2020-01-30T00:00:00Z"), "policyLevel" : "B" }
+#{ "_id" : ISODate("2020-01-31T00:00:00Z"), "policyLevel" : "B" }
+#{ "_id" : ISODate("2020-02-01T00:00:00Z"), "policyLevel" : "C" }
+#{ "_id" : ISODate("2020-02-02T00:00:00Z"), "policyLevel" : "B'" }
+#{ "_id" : ISODate("2020-02-03T00:00:00Z"), "policyLevel" : "A'" }
 
 # guillaume at mongodb.com 20200630 ($project stage by Sylvain Chambon)
 
@@ -37,11 +36,11 @@ import string
 START_TIME = datetime.fromisoformat("2020-01-15")
 END_TIME = datetime.fromisoformat("2020-03-15")
 
-# MongoDB aggregation pipeline to extract higher tariff level per day
+# MongoDB aggregation pipeline to extract higher policy level per day
 pipeline=[
         {
                 "$project" : {
-                        "tariff" : 1,
+                        "policy" : 1,
                         "level" : 1,
                         "days" : {
                                 "$map" : {
@@ -93,7 +92,7 @@ pipeline=[
         {
                 "$group" : {
                         "_id" : "$days",
-                        "tariffLevel" : {
+                        "policyLevel" : {
                                 "$max" : "$level"
                         }
                 }
@@ -113,11 +112,11 @@ pipeline=[
         }
 ]
 
-# Extract data for daily tariff level from  MongoDB
+# Extract data for daily policy level from  MongoDB
 connection = pymongo.MongoClient()
 res = list(connection.test.policies.aggregate(pipeline))
 
-# Computes re-appearance of tariff level (A,A',A'', etc.)
+# Computes re-appearance of policy level (A,A',A'', etc.)
 levels={}
 firstLevel=""
 for doc in res:
